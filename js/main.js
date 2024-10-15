400c8c9ff5e0ef9314e801d4a01c6c0bd4cdb452
Meelis Luks a.k.a Mella <mella@lux.ee>
2024-10-15 20:31:49 +0300

    more fixes

--- js/main.js
+++ js/main.js
@@ -161,13 +161,13 @@ $(document).ready(function() {
         resetStates();
     });
 
-    // Register service worker
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-            console.log('Service worker registered with scope:', registration.scope);
-        }).catch(function(error) {
-            console.log('Service worker registration failed:', error);
-        });
-    }
-
 });
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+        console.log('Service worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+        console.log('Service worker registration failed:', error);
+    });
+}
